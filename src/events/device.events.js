f08e9b6494cc331998785a4cf55aaa6255b61935
Arun Anil <arunanil@Aruns-MacBook-Air.local>
2021-09-20 21:37:41 +0530

    fix : fixed device event

--- src/events/device.events.js
+++ src/events/device.events.js
@@ -3,14 +3,13 @@ const aedesService = require("../services/mqtt.services");
 
 exports.deviceUpdated = (device) => {
   if (device) {
+    delete device.secret;
     aedesService.publishToTopic(
       `${device.account_id}/${device.section_id}/${device.id}`,
       {
-        message: device.name,
-        channels: device.channels.map((d) => ({
-          value: d.value,
-        })),
-      }
-    );
+        data: device
+      })
   }
+    );
+}
 };
