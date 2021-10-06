9ba6632e7668614658d1bf73c2c1c67c7fc01f3a
arunneoito <63841234+arunneoito@users.noreply.github.com>
2021-10-06 17:18:31 +0530

    test publish mqtt

--- src/events/device.events.js
+++ src/events/device.events.js
@@ -9,8 +9,8 @@ exports.deviceUpdated = (device) => {
     const data = device.channels.map((d) => ({
       port: d.port,
       value: d.value,
-      valueType: d.value_type,
-      inout: d.inout,
+      // valueType: d.value_type,
+      // inout: d.inout,
     }));
 
     delete device.secret;
@@ -18,8 +18,7 @@ exports.deviceUpdated = (device) => {
       // eslint-disable-next-line no-underscore-dangle
       helpers.getDeviceTopic(device),
       {
-        data:data,
-        deviceId: device._id.toString(),
+        data
       }
     );
   }
