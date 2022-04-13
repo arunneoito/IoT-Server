7525950c80366411bef55bde921278dce2c419e6
arunanil <arun.a@neoito.com>
2022-04-13 15:28:43 +0530

    feat: google account linking

--- src/routes/google-fullfilment.routes.js
+++ src/routes/google-fullfilment.routes.js
@@ -62,23 +62,29 @@ async function getUserIdOrThrow(headers) {
 function parseHomeGraphDevice(devices) {
   const hgDevices = [];
   devices.forEach((element) => {
-    element.channels.forEach((channel) => {
+    element.channels.forEach((channel, i) => {
       hgDevices.push({
         id: channel.id,
         type: "action.devices.types.OUTLET",
         traits: ["action.devices.traits.OnOff"],
         name: {
-          defaultNames: "neoswitch socket",
+          defaultNames: ["neo-switch-1"],
           name: channel.name,
+          nicknames: ["wall plug"],
         },
+        willReportState: false,
+        roomHint: element.section_id,
         deviceInfo: {
           manufacturer: "Neoito",
-          model: "neo-sw-p4",
+          model: "neo-switch-p4",
           hwVersion: "1.0",
           swVersion: "1.0",
         },
-        willReportState: true,
-        customData: {},
+        otherDeviceIds: [
+          {
+            deviceId: "local-device-id",
+          },
+        ],
       });
     });
   });
@@ -98,71 +104,7 @@ app.onSync(async (body, headers) => {
     requestId: body.requestId,
     payload: {
       agentUserId: user.id,
-      devices: [
-        {
-          id: "123",
-          type: "action.devices.types.OUTLET",
-          traits: ["action.devices.traits.OnOff"],
-          name: {
-            defaultNames: ["My Outlet 1234"],
-            name: "Night light",
-            nicknames: ["wall plug"],
-          },
-          willReportState: false,
-          roomHint: "kitchen",
-          deviceInfo: {
-            manufacturer: "lights-out-inc",
-            model: "hs1234",
-            hwVersion: "3.2",
-            swVersion: "11.4",
-          },
-          otherDeviceIds: [
-            {
-              deviceId: "local-device-id",
-            },
-          ],
-          customData: {
-            fooValue: 74,
-            barValue: true,
-            bazValue: "foo",
-          },
-        },
-        {
-          id: "456",
-          type: "action.devices.types.LIGHT",
-          traits: [
-            "action.devices.traits.OnOff",
-            "action.devices.traits.Brightness",
-            "action.devices.traits.ColorSetting",
-          ],
-          name: {
-            defaultNames: ["lights out inc. bulb A19 color hyperglow"],
-            name: "lamp1",
-            nicknames: ["reading lamp"],
-          },
-          willReportState: false,
-          roomHint: "office",
-          attributes: {
-            colorModel: "rgb",
-            colorTemperatureRange: {
-              temperatureMinK: 2000,
-              temperatureMaxK: 9000,
-            },
-            commandOnlyColorSetting: false,
-          },
-          deviceInfo: {
-            manufacturer: "lights out inc.",
-            model: "hg11",
-            hwVersion: "1.2",
-            swVersion: "5.4",
-          },
-          customData: {
-            fooValue: 12,
-            barValue: false,
-            bazValue: "bar",
-          },
-        },
-      ],
+      devices,
     },
   };
   console.log("SyncResponse:", syncResponse);
