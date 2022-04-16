271b3d1d733377d4110f0ad1e2a1a9799b75fe92
Arun Anil <arunanilsmail@gmail.com>
2022-04-16 09:14:36 +0530

    feat: google account linking

--- src/routes/google-fullfilment.routes.js
+++ src/routes/google-fullfilment.routes.js
@@ -206,11 +206,8 @@ app.onExecute(async (body, headers) => {
         console.log("ReportStateResponse:", reportStateResponse);
       } catch (e) {
         console.log(e);
-        const errorResponse = JSON.parse(e);
-        console.log(
-          "error reporting device state to homegraph:",
-          errorResponse
-        );
+
+        console.log("error reporting device state to homegraph:", e);
       }
     } catch (e) {
       console.log(
